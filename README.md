0fcc463e9fd63fa358022e371e8d47bb4e05f8ef
Bruno Bandeira Fernandes <brunobandeirafernandes@gmail.com>
2024-08-25 16:08:56 -0300

    Initial Project

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+npm i typescript tsx tsup vitest -D
+npm i express cors zod 
+npx tsc --init
+npm i @types/node @types/express
+npm i @types/cors -D
+
+
+## Sugestão do Copilot no Mac
+- option barra invertida
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+// Create a REST API server using Express
+import express from 'express';
+import { Request, Response } from 'express';
+import cors from 'cors';
+
+const PORT = 3000;
+const app = express();
+// Implement the most common middleware here, for example body-parser, cors, etc
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
